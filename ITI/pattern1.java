7318586b08247db14df10d7b73f783bb377d4e30
Mahmoud Fawzy Taha-Elaraby <1998mft1998@gmail.com>
2024-08-22 13:26:34 +0300

    adding some scripts to create patterns with stars

---
+++ ITI/pattern1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter side length: ");
+        int n = in.nextInt();
+        for (int i=n; i>0; i--){ // row (line)
+            for (int j=0; j<i; j++){ // column
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }//end of main
+}//end of class
---
+++ ITI/pattern2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter side length: ");
+        int n = in.nextInt();
+        for (int i=0; i<=n; i++){ // row (line)
+            for (int j=0; j<i; j++){ // column
+                System.out.print(" ");
+            }
+            for (int j=0; j<n-i; j++){ // column
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }//end of main
+}//end of class
